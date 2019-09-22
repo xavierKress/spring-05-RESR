461770a99d7776230747bda1568509d2b40ae213
xavier <xavier.kress@gmail.com>
2019-09-22 15:43:09 +0200

    fix error

--- src/main/java/com/demospring/xavierkress/spring_initializr/controllers/PublisherRestController.java
+++ src/main/java/com/demospring/xavierkress/spring_initializr/controllers/PublisherRestController.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +21,7 @@ public class PublisherRestController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ApiOperation("Get all publisher")
-    public ResponseEntity getPublishers(Model model){
-        model.addAttribute("books", publisherRepository.findAll());
+    public ResponseEntity getPublishers(){
         return new ResponseEntity(publisherRepository.findAll(), HttpStatus.OK);
     }
 }
